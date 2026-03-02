605942966454cbf1533b89a2dd0003a33c6558de
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-03-02 13:31:33 -0300

    feat: Expose BrickEngine and p5 to the global window object.

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,9 +1,17 @@
 import * as BrickEngine from "brick-engine-js";
 import { bootstrap } from "brick-engine-js";
+import p5 from "p5";
 import GameMenu from "./core/GameMenu";
 import VConsole from "vconsole";
 
 const vConsole = new VConsole();
+
+try {
+  (window as any).BrickEngine = BrickEngine;
+  (window as any).p5 = p5;
+} catch (error) {
+  console.error(error);
+}
 
 const p5Instance = bootstrap(GameMenu);
 
