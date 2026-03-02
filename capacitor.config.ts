2d9e5571cc3d3d2f552c26d5c99bebbbfbc645af
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-03-01 21:58:40 -0300

    chore: Update Capacitor web directory from 'www' to 'dist'.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.brickengine.hub',
-  appName: 'brick-engine-menu',
-  webDir: 'www'
+  appId: "com.brickengine.hub",
+  appName: "brick-engine-menu",
+  webDir: "dist",
 };
 
 export default config;
