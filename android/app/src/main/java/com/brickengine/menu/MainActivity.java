e2dd730248cf903607808edde2acc53902bd4a5e
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-03-01 21:48:24 -0300

    chore: setup ionic capacitor for android

---
+++ android/app/src/main/java/com/brickengine/menu/MainActivity.java
@@ -0,0 +1,5 @@
+package com.brickengine.menu;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.brickengine.menu',
+  appName: 'brick-engine-menucom.brickengine.menudist',
+  webDir: 'dist'
+};
+
+export default config;
