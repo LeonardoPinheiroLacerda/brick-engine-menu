9ad7bd06ccdbe873854e6ddda2d5ceb7610dde4e
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-03-02 12:53:50 -0300

    feat: Add VConsole for in-browser debugging.

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,6 +2,9 @@ import * as BrickEngine from "brick-engine-js";
 import { bootstrap } from "brick-engine-js";
 import p5 from "p5";
 import GameMenu from "./core/GameMenu";
+import VConsole from "vconsole";
+
+const vConsole = new VConsole();
 
 (window as any).BrickEngine = BrickEngine;
 (window as any).p5 = p5;
