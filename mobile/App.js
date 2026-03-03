1619409fba205ef5dfc8ddee78cfcb2ed1356213
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2026-03-03 12:20:13 -0300

    feat: Implement safe area handling using `react-native-safe-area-context` to improve layout and update dependencies.

--- mobile/App.js
+++ mobile/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { StyleSheet, View, StatusBar, Platform } from "react-native";
 import { WebView } from "react-native-webview";
 import * as NavigationBar from "expo-navigation-bar";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   useEffect(() => {
@@ -12,31 +13,40 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" backgroundColor="#30b3ff" />
-      <WebView
-        source={{
-          uri: `https://LeonardoPinheiroLacerda.github.io/brick-engine-menu?t=${new Date().getTime()}`,
-        }}
-        style={styles.webview}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-        scalesPageToFit={true}
-        allowsFullscreenVideo={true}
-        incognito={true}
-        cacheEnabled={false}
-        cacheMode="LOAD_NO_CACHE"
-      />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.outerContainer}>
+        <StatusBar barStyle="light-content" backgroundColor="#30b3ff" />
+        <SafeAreaView
+          style={styles.container}
+          edges={["top", "bottom", "left", "right"]}
+        >
+          <WebView
+            source={{
+              uri: `https://LeonardoPinheiroLacerda.github.io/brick-engine-menu?t=${new Date().getTime()}`,
+            }}
+            style={styles.webview}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            startInLoadingState={true}
+            scalesPageToFit={true}
+            allowsFullscreenVideo={true}
+            incognito={true}
+            cacheEnabled={false}
+            cacheMode="LOAD_NO_CACHE"
+          />
+        </SafeAreaView>
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  outerContainer: {
     flex: 1,
     backgroundColor: "#30b3ff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  container: {
+    flex: 1,
   },
   webview: {
     flex: 1,
